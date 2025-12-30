b8d97b1c970137906acc95cd388279d513951def
PhamKhaVy <phamkhavycm2004@gmail.com>
2025-12-31 01:53:00 +0700

    Add: Cart Item Selection BE

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,6 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
-  app.enableCors({
-    origin: '*', // In production, specify exact origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -24,7 +17,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
